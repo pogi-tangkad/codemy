fd6ec36659d7e57a363d0c24cfee7955b93929f1
Kevin Repking <pogi.tangkad@gmail.com>
2021-02-25 16:22:20 +0900

    Still learning sqlite3

--- sqlite3/sqlite3-testing.py
+++ sqlite3/sqlite3-testing.py
@@ -8,7 +8,10 @@ conn = sqlite3.connect('customer.db')
 # Create a cursor to move through database
 c = conn.cursor()
 
-# Create a table
+# Remove existing table to keep using same .py file
+c.execute("DROP TABLE IF EXISTS customers")
+
+# Create a table with a default value set for email
 ''' Data Types:
     NULL    - Does or does not exist
     INTEGER - Whole number
@@ -17,23 +20,39 @@ c = conn.cursor()
     BLOB    - As is file (images, music)
 '''
 c.execute("""CREATE TABLE customers (
-    first_name text,
-    last_name text,
-    email text
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT DEFAULT 'no email given'
     )""")
 
-# Create List of values to put into the Table
+conn.commit()
+
+# Create List of values to put into the Table via executemany
 many_customers = [
     ('John', 'Elder', 'john@codemy.com'),
     ('Tim', 'Wonky', 'twonk@codemy.com'),
-    ('Mary', 'Schmuckenhoffer', 'mary@codemy.com')
+    ('Mary', 'Schmuckenhoffer', 'mary@codemy.com'),
+    ('Darryl', 'Funbags', 'dbag@yabbadabba.com'),
+    ('Paula', 'Poundstone', 'poundstone@retroencabulator.com'),
+    ('Fuh', 'Kyu', 'kyu@yabbadabba.com')
     ]
 
 c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+conn.commit()
+
+# Change the first_name of one of the entries matching a last_name value
+c.execute(""" UPDATE customers SET first_name = 'Lin'
+            WHERE last_name = 'Kyu'
+    """)
+conn.commit()
+
+# Insert a single entry making sure to use the DEFAULT value for email
+c.execute("INSERT INTO customers (first_name, last_name) VALUES ('Mark', 'Elberg')")
+
 
 # Query the database
-c.execute("SELECT first_name FROM customers")
-print(c.fetchall())
+#c.execute("SELECT first_name FROM customers")
+#print(c.fetchall())
 
 # A new execute-select needs to be implemented after a fetch
 # to move the 'cursor' back to the beginning if needed
@@ -41,13 +60,16 @@ c.execute("SELECT * FROM customers")
 #print(c.fetchone()[1])
 #test_fetch = c.fetchmany(1)
 #print(test_fetch)
+#c.execute("SELECT * FROM customers WHERE last_name LIKE 'El%' ")
 items = c.fetchall()
 #first_names = []
-print(f'NAME\t\t\tEMAIL')
+print(f'\nNAME\t\t\tEMAIL')
 print(f'------\t\t\t--------')
 for item in items:
     full_name = item[0] + ' ' + item[1]
-    if len(full_name) <= 12:
+    if len(full_name) <= 7:
+        print(f'{full_name}\t\t\t{item[2]}')
+    elif len(full_name) <= 14:
         print(f'{full_name}\t\t{item[2]}')
     else:
         print(f'{full_name}\t{item[2]}')
@@ -59,7 +81,6 @@ I am assuming that the following 'commit' and 'close' are going to be necessary
 when using sqlite3 within another program where the main program will still be
 running after we are done with the database.
 '''
-
 # Commit the executed commands (maybe not needed? possibly best practice like 'close')
 conn.commit()
 
