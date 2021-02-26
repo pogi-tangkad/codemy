44d21bad56abe4951e3690e3837d4c2ecb9cb517
Kevin Repking <pogi.tangkad@gmail.com>
2021-02-26 10:02:49 +0900

    Cleaned up delete_one function - 26Feb21

--- sqlite3/our_db.py
+++ sqlite3/our_db.py
@@ -5,7 +5,6 @@ import sqlite3
 def connect_db():
     # Connect to a database named 'customer.db' and/or create the database if it doesn't exist already
     conn = sqlite3.connect('customer.db')
-
     # Create a cursor to move through database
     c = conn.cursor()
     return conn, c
@@ -13,7 +12,6 @@ def connect_db():
 def close_db(conn):
     # Commit the executed commands (maybe not needed? possibly best practice like 'close')
     conn.commit()
-
     # Close the connection to database (best practice to do so explicitly)
     conn.close()
 
@@ -24,7 +22,7 @@ def show_all():
     c.execute("SELECT rowid,* FROM customers ORDER BY last_name,first_name")
     items = c.fetchall()
     print(f'\nID\tNAME\t\t\tEMAIL')
-    print(f'------\t\t\t--------')
+    print(f'-----\t------\t\t\t--------')
     for item in items:
         full_name = item[1] + ' ' + item[2]
         if len(full_name) <= 7:
@@ -42,29 +40,15 @@ def add_one(first, last, email):
     close_db(conn)
 
 def delete_one():
-    conn, c = connect_db()
     show_all()
-    # Get all of the row IDs in a list
-    c.execute("SELECT rowid FROM customers")
-    id_list = c.fetchall()
-    id_check = []
-    for id_num in id_list:
-        id_check.append(id_num[0])
-    #print(id_check)
-    
+    conn, c = connect_db()
     id_q = input('\nChoose "ID" to delete:  ')
-    try:
-        int(id_q)
-    except:
-        print(f'\n{id_q} is not a valid selection.')
-        return
-
-    if int(id_q) in id_check:
-        id = int(id_q)
-        c.execute("DELETE FROM customers WHERE rowid=?", (id,))
+    c.execute("SELECT rowid FROM customers WHERE rowid=(?)", (id_q,))
+    if c.fetchall():
+        c.execute("DELETE FROM customers WHERE rowid=(?)", (id_q,))
         conn.commit()
         show_all()
     else:
-        print(f'\n{id_q} not a valid selection.')
+        print(f'\n!ERROR!:  {id_q} is not a valid selection.')
 
     close_db(conn)
